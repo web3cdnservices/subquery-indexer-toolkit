da5379330f983cbe0ccf02ddf0a8ae6bd72f9445
Liver-23 <liver23@gmail.com>
2024-01-26 20:05:40 +0200

    Update get_indexer_address_from_proxy_endpoint.sh

--- src/docker/sh_exporter/scripts/lib/get_indexer_address_from_proxy_endpoint.sh
+++ src/docker/sh_exporter/scripts/lib/get_indexer_address_from_proxy_endpoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INDEXER_ADDR=`curl -s http://indexer_proxy:80/healthy | jq -r ".indexer"`
+INDEXER_ADDR=`curl -s http://indexer_proxy:8375/healthy | jq -r ".indexer"`
 
 echo $INDEXER_ADDR
 exit 0
